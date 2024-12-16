b59f747ad4c1f355f774c5d68df7d0b78aa9c8a2
krischal07 <krischal.shrestha9849@gmail.com>
2024-12-16 17:09:12 +0545

    added layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,6 +5,7 @@ import Footer from '../footer/Footer'
 const Layout = ({children}) => {
   return (
     <div>
+        
         <Navbar />
         <div className='content'>
             {children}
