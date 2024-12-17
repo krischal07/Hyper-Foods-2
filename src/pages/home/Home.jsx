86e7272666f320219cec22b2f6aedb96c766356b
krischal07 <krischal.shrestha9849@gmail.com>
2024-12-17 20:21:45 +0545

    created until footer page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext'
 import HeroSection from '../../components/heroSection/HeroSection'
 import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard'
+import Testimonial from '../../components/testimonial/Testimonial'
 
 const Home = () => {
     const context = useContext(myContext)
@@ -14,6 +16,8 @@ const Home = () => {
     <Layout>
         <HeroSection />
         <Filter />
+        <ProductCard />
+        <Testimonial />
     </Layout>
   )
 }
