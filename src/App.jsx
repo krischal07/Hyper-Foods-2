4829b62d43a05ba56ebed6e99927a6d3b64dd7a1
krischal07 <krischal.shrestha9849@gmail.com>
2024-12-18 20:55:58 +0545

    added protected route4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Order from "./pages/order/Order"
 import Cart from "./pages/cart/Cart"
@@ -22,14 +22,30 @@ const App = () => {
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/order' element={<Order />} />
+          <Route path='/order' element={
+            <ProtectedRoute>
+              <Order />
+            </ProtectedRoute>
+          } />
           <Route path='/cart' element={<Cart />} />
-          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/dashboard' element={
+            <ProtectedRouteForAdmin>
+              <Dashboard />
+            </ProtectedRouteForAdmin>
+          } />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/productInfo/:id' element={<Productinf />} />
-          <Route path='/addproduct' element={<AddProduct />} />
-          <Route path='/updateproduct' element={<UpdateProduct />} />
+          <Route path='/addproduct' element={
+            <ProtectedRouteForAdmin>
+              <AddProduct />
+            </ProtectedRouteForAdmin>
+          } />
+          <Route path='/updateproduct' element={
+            <ProtectedRouteForAdmin>
+              <UpdateProduct />
+            </ProtectedRouteForAdmin>
+          } />
 
           <Route path='/*' element={<NoPage />} git/>
         </Routes>
@@ -41,3 +57,27 @@ const App = () => {
 }
 
 export default App
+
+//user
+
+export const ProtectedRoute = ({children})=>{
+  const user = localStorage.getItem('user')
+  if(user){
+    return children
+  }
+  else{
+    return <Navigate to=<Login /> />
+  }
+}
+
+//admin
+const ProtectedRouteForAdmin = ({children})=>{
+  const admin = JSON.parse(localStorage.getItem('user'))
+  if(admin.user.email === 'admin@gmail.com'){
+    return children
+  }
+  else{
+    return <Navigate to={<Login />}/>
+  }
+
+}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,6 +13,15 @@ export default function Navbar() {
   const context = useContext(myContext)
   const { toggleMode, mode } = context
 
+  const user = JSON.parse(localStorage.getItem('user'))
+  console.log(user.user.email)
+
+  const logout = ()=>{
+    localStorage.clear('user')
+    window.location.href = '/login'
+    console.log("logout clikced")
+  }
+
   return (
     <div className="bg-white sticky top-0 z-50  "  >
       {/* Mobile menu */}
@@ -62,14 +71,17 @@ export default function Navbar() {
                     </Link>
                   </div>
 
-                  <div className="flow-root">
+                  {user.user.email==='admin@gmail.com'?<div className="flow-root">
                     <Link to={'/dashboard'} className="-m-2 block p-2 font-medium text-gray-900" style={{ color: mode === 'dark' ? 'white' : '', }}>
                       admin
                     </Link>
                   </div>
-
+                  :""}
+                  
                   <div className="flow-root">
-                    <a className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer" style={{ color: mode === 'dark' ? 'white' : '', }}>
+                    <a className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer" style={{ color: mode === 'dark' ? 'white' : '', }}
+                    onClick={logout}
+                    >
                       Logout
                     </a>
                   </div>
@@ -139,11 +151,14 @@ export default function Navbar() {
                   <Link to={'/order'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
                     Order
                   </Link>
-                  <Link to={'/dashboard'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
+                  {user.user.email==='admin@gmail.com'?<Link to={'/dashboard'} className="text-sm font-medium text-gray-700 " style={{ color: mode === 'dark' ? 'white' : '', }}>
                     Admin
-                  </Link>
+                  </Link>:""}
+                  
 
-                  <a className="text-sm font-medium text-gray-700 cursor-pointer  " style={{ color: mode === 'dark' ? 'white' : '', }}>
+                  <a className="text-sm font-medium text-gray-700 cursor-pointer  " style={{ color: mode === 'dark' ? 'white' : '', }}
+                  
+                  onClick={logout}>
                     Logout
                   </a>
                 </div>
