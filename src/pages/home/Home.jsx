3c3e22e57a7256c59683e3093a26e4e85b9e5789
krischal07 <krischal.shrestha9849@gmail.com>
2024-12-18 16:21:13 +0545

    added redux

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,15 +5,31 @@ import HeroSection from '../../components/heroSection/HeroSection'
 import Filter from '../../components/filter/Filter'
 import ProductCard from '../../components/productCard/ProductCard'
 import Testimonial from '../../components/testimonial/Testimonial'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart, deleteFromCart } from '../../redux/CartSlice'
 
 const Home = () => {
     const context = useContext(myContext)
-    console.log(context)
-    // const {name, rollno} = context
-    // const {state, color} = context
+  
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state) => state.cart)
 
+  console.log(cartItem)
+  const addCart = ()=>{
+    dispatch(addToCart('shirt'))
+  }
+  
+  const deleteCart = ()=>{
+    dispatch(deleteFromCart('shirt'))
+
+  }
   return (
     <Layout>
+        <div className='flex gap-5 justify-center'>
+        <button onClick={addCart}>add</button>
+        <button onClick={deleteCart}>delete</button>
+          
+        </div>
         <HeroSection />
         <Filter />
         <ProductCard />
