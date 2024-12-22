ecde3f88952e84b2fed2f907daa513ca5dd3a05f
Arpit Adhikari <arpitadhikari248@gmail.com>
2024-12-22 22:00:47 +0545

    signup login

--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -3,33 +3,42 @@ import { Link } from 'react-router-dom'
 function Login() {
    
     return (
-        <div className=' flex justify-center items-center h-screen bg-yellow-500'>
-            <div className=' bg-blue-950 px-10 py-10 rounded-xl '>
+        <div className=' flex justify-center items-center h-screen bg-white'>
+            <div className=' bg-white px-10 py-10 rounded-xl '>
+                {/* logo */}
+            <div className="flex justify-center mb-4">
+               <img 
+                  src="/src/assets/hyperlogo.png" 
+                  alt="App Logo" 
+                  className="w-30 h-20 object-contain"
+               />
+            </div>
+
                 <div className="">
-                    <h1 className='text-center text-white text-xl mb-4 font-bold'>Login</h1>
+                    <h1 className='text-center text-black text-xl mb-4 font-bold'>Login</h1>
                 </div>
                 <div>
                     <input type="email"
                         name='email'
-                        className=' bg-white-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-blue-950 placeholder:text-gray-500 outline-none'
+                        className=' bg-gray-400 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-blue-950 placeholder:text-black outline-none'
                         placeholder='Email'
                     />
                 </div>
                 <div>
                     <input
                         type="password"
-                        className=' bg-white-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-blue-950 placeholder:text-gray-500 outline-none'
+                        className=' bg-gray-400 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-blue-950 placeholder:text-black outline-none'
                         placeholder='Password'
                     />
                 </div>
                 <div className=' flex justify-center mb-3'>
                     <button
-                        className=' bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
+                        className=' bg-blue-800 w-full text-white font-bold  px-2 py-2 rounded-lg'>
                         Login
                     </button>
                 </div>
                 <div>
-                    <h2 className='text-white'>Don't have an account <Link className=' text-yellow-500 font-bold' to={'/signup'}>Signup</Link></h2>
+                    <h2 className='text-black'>Don't have an account? <Link className=' text-blue-800 font-bold' to={'/signup'}>Signup</Link></h2>
                 </div>
             </div>
         </div>
