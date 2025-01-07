f4a290c76a31d85cb66ee5722a7b7e25d2ab80bf
Arpit Adhikari <arpitadhikari248@gmail.com>
2025-01-07 19:25:43 +0545

    icon

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -161,7 +161,7 @@ export default function Modal({
                                 class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                                 type="button"
                               >
-                                Bank
+                               <i class="fas fa-university"></i> 
                               </button>
 
                               <button
@@ -178,7 +178,7 @@ export default function Modal({
                                 class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                                 type="button"
                               >
-                                COD
+                                <i class="fas fa-wallet"></i>
                               </button>
                             </div>
                           )}
