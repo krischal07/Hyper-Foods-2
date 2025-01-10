b828040c5d6823c84085087d296655f10f434a50
krischal07 <krischal.shrestha9849@gmail.com>
2025-01-10 15:21:03 +0545

    cart slice name edited

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { configureStore } from '@reduxjs/toolkit'
-import cartSlice from "./cartSlice"
+import React from "react";
+import { configureStore } from "@reduxjs/toolkit";
+// import cartSlice from "./cartSlice"k
+import cartSlice from "./CartSlice";
 
 const store = configureStore({
-    reducer: {
-        cart : cartSlice
-    },
-    devTools: true
-})
-export default store
+  reducer: {
+    cart: cartSlice,
+  },
+  devTools: true,
+});
+export default store;
