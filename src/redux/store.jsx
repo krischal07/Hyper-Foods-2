c87f3dd7d5c0e762a8751861e0705710c29390ad
krischal07 <krischal.shrestha9849@gmail.com>
2025-01-10 15:31:17 +0545

    cartslice.jsx

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./CartSlice";
+import cartSlice from "./CartSlice.jsx";
 
 const store = configureStore({
   reducer: {
